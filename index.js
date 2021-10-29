75f30f40eeb1d433dc0693b73b0094801c19edb7
unknown <sjjoseph106@gmail.com>
2021-10-29 13:59:29 +0900

    케미 이름 추가

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ var q = {
     12: {"title": "문제 12번", "type": "JP", "A": "J", "B": "P"}
 }
 var result = {
-    "ISTJ": {"character": "우왁굳", "explain": "ISTJ 설명", "img1": "static/img/istj.jpg", "img2": "static/img/esfj.png", "img3": "static/img/enfj.jpg"},
-    "ISFJ": {"character": "미츠네 하쿠", "explain": "ISFJ 설명", "img1": "static/img/isfj.jpg", "img2": "static/img/esfp.png", "img3": "static/img/entj.jpg"},
-    "ESTJ": {"character": "이덕수 할아바이", "explain": "ESTJ 설명", "img1": "static/img/estj.png", "img2": "static/img/isfp.jpg", "img3": "static/img/infj.jpg"},
-    "ESFJ": {"character": "엔젤님", "explain": "ESFJ 설명", "img1": "static/img/esfj.png", "img2": "static/img/istj.jpg", "img3": "static/img/intj.png"},
-    "ISTP": {"character": "새우튀김", "explain": "ISTP 설명", "img1": "static/img/istp.png", "img2": "static/img/enfj.jpg", "img3": "static/img/enfp.jpg"},
-    "ISFP": {"character": "부정형 인간", "explain": "ISFP 설명", "img1": "static/img/isfp.jpg", "img2": "static/img/estj.png", "img3": "static/img/entp.png"},
-    "ESTP": {"character": "김치만두번영택사스가", "explain": "ESTP 설명", "img1": "static/img/estp.png", "img2": "static/img/istj.jpg", "img3": "static/img/infp.jpg"},
-    "ESFP": {"character": "독고혜지", "explain": "ESFP 설명", "img1": "static/img/esfp.png", "img2": "static/img/isfj.jpg", "img3": "static/img/intp.jpg"},
-    "INFP": {"character": "뢴트게늄 & 아이네", "explain": "INFP 설명", "img1": "static/img/infp.jpg", "img2": "static/img/entj.jpg", "img3": "static/img/estp.png"},
-    "INTP": {"character": "주르르", "explain": "INTP 설명", "img1": "static/img/intp.jpg", "img2": "static/img/enfj.jpg", "img3": "static/img/esfp.png"},
-    "ENFP": {"character": "릴파", "explain": "ENFP 설명", "img1": "static/img/enfp.jpg", "img2": "static/img/intj.png", "img3": "static/img/istp.png"},
-    "ENTP": {"character": "풍신", "explain": "ENTP 설명", "img1": "static/img/entp.png", "img2": "static/img/infj.jpg", "img3": "static/img/isfp.jpg"},
-    "INFJ": {"character": "징버거 & 비챤", "explain": "INFJ 설명", "img1": "static/img/infj.jpg", "img2": "static/img/entp.png", "img3": "static/img/estj.png"},
-    "INTJ": {"character": "냉참", "explain": "INTJ 설명", "img1": "static/img/intj.png", "img2": "static/img/enfp.jpg", "img3": "static/img/esfj.png"},
-    "ENFJ": {"character": "도파민 박사", "explain": "ENFJ 설명", "img1": "static/img/enfj.jpg", "img2": "static/img/istp.png", "img3": "static/img/istj.jpg"},
-    "ENTJ": {"character": "고세구", "explain": "ENTJ 설명", "img1": "static/img/entj.jpg", "img2": "static/img/infp.jpg", "img3": "static/img/isfj.jpg"}
+    "ISTJ": {"character": "우왁굳", "explain": "ISTJ 설명", "img1": "static/img/istj.jpg", "img2": "static/img/esfj.png", "img3": "static/img/enfj.jpg", "good": "엔젤님", "bad": "도파민 박사"},
+    "ISFJ": {"character": "미츠네 하쿠", "explain": "ISFJ 설명", "img1": "static/img/isfj.jpg", "img2": "static/img/esfp.png", "img3": "static/img/entj.jpg", "good": "독고혜지", "bad": "고세구"},
+    "ESTJ": {"character": "이덕수 할아바이", "explain": "ESTJ 설명", "img1": "static/img/estj.png", "img2": "static/img/isfp.jpg", "img3": "static/img/infj.jpg", "good": "부정형 인간", "bad": "징버거 & 비챤"},
+    "ESFJ": {"character": "엔젤님", "explain": "ESFJ 설명", "img1": "static/img/esfj.png", "img2": "static/img/istj.jpg", "img3": "static/img/intj.png", "good": "우왁굳", "bad": "냉참"},
+    "ISTP": {"character": "새우튀김", "explain": "ISTP 설명", "img1": "static/img/istp.png", "img2": "static/img/enfj.jpg", "img3": "static/img/enfp.jpg", "good": "도파민 박사", "bad": "릴파"},
+    "ISFP": {"character": "부정형 인간", "explain": "ISFP 설명", "img1": "static/img/isfp.jpg", "img2": "static/img/estj.png", "img3": "static/img/entp.png", "good": "이덕수 할아바이", "bad": "풍신"},
+    "ESTP": {"character": "김치만두번영택사스가", "explain": "ESTP 설명", "img1": "static/img/estp.png", "img2": "static/img/istj.jpg", "img3": "static/img/infp.jpg", "good": "우왁굳", "bad": "뢴트게늄 & 아이네"},
+    "ESFP": {"character": "독고혜지", "explain": "ESFP 설명", "img1": "static/img/esfp.png", "img2": "static/img/isfj.jpg", "img3": "static/img/intp.jpg", "good": "미츠네 하쿠", "bad": "주르르"},
+    "INFP": {"character": "뢴트게늄 & 아이네", "explain": "INFP 설명", "img1": "static/img/infp.jpg", "img2": "static/img/entj.jpg", "img3": "static/img/estp.png", "good": "고세구", "bad": "김치만두번영택사스가"},
+    "INTP": {"character": "주르르", "explain": "INTP 설명", "img1": "static/img/intp.jpg", "img2": "static/img/enfj.jpg", "img3": "static/img/esfp.png", "good": "도파민 박사", "bad": "독고혜지"},
+    "ENFP": {"character": "릴파", "explain": "ENFP 설명", "img1": "static/img/enfp.jpg", "img2": "static/img/intj.png", "img3": "static/img/istp.png", "good": "냉참", "bad": "새우튀김"},
+    "ENTP": {"character": "풍신", "explain": "ENTP 설명", "img1": "static/img/entp.png", "img2": "static/img/infj.jpg", "img3": "static/img/isfp.jpg", "good": "징버거 & 비챤", "bad": "부정형 인간"},
+    "INFJ": {"character": "징버거 & 비챤", "explain": "INFJ 설명", "img1": "static/img/infj.jpg", "img2": "static/img/entp.png", "img3": "static/img/estj.png", "good": "풍신", "bad": "이덕수 할아바이"},
+    "INTJ": {"character": "냉참", "explain": "INTJ 설명", "img1": "static/img/intj.png", "img2": "static/img/enfp.jpg", "img3": "static/img/esfj.png", "good": "릴파", "bad": "엔젤님"},
+    "ENFJ": {"character": "도파민 박사", "explain": "ENFJ 설명", "img1": "static/img/enfj.jpg", "img2": "static/img/istp.png", "img3": "static/img/istj.jpg", "good": "새우튀김", "bad": "우왁굳"},
+    "ENTJ": {"character": "고세구", "explain": "ENTJ 설명", "img1": "static/img/entj.jpg", "img2": "static/img/infp.jpg", "img3": "static/img/isfj.jpg", "good": "뢴트게늄 & 아이네", "bad": "미츠네 하쿠"}
 }
 function start() { 
     $(".start").hide();
@@ -57,10 +57,13 @@ function next() {
         ($('#JP').val() < 2) ? mbti += "P" : mbti += "J";
 
         $("#img1").attr("src", result[mbti]["img1"]);
-        $("#img2").attr("src", result[mbti]["img2"]);
-        $("#img3").attr("src", result[mbti]["img3"]);
         $("#character").html(result[mbti]["character"]);
         $("#explain").html(result[mbti]["explain"]);
+        $("#img2").attr("src", result[mbti]["img2"]);
+        $("#img3").attr("src", result[mbti]["img3"]);
+        $("#good").html(result[mbti]["good"]);
+        $("#bad").html(result[mbti]["bad"]);
+
 
     } else {
         $(".progress-bar").attr('style','width: calc(100/12*'+num+'%)');
