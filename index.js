fb6e01ef895892e89984e6e5e13041ba9dea17da
unknown <sjjoseph106@gmail.com>
2021-11-03 16:56:51 +0900

    추정 예외 처리

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ var q = {
     12: {"title": "문제 12번", "type": "JP", "A": "J", "B": "P"}
 }
 var result = {
-    "ISTJ": {"character": "우왁굳", "mbti": "당신은 혹시... ISTJ?", "explain": "ISTJ 설명", "img1": "static/img/istj.jpg", "img2": "static/img/esfj.png", "img3": "static/img/enfj.jpg", "good": "엔젤님", "bad": "도파민 박사"},
-    "ISFJ": {"character": "미츠네 하쿠", "mbti": "당신은 혹시... ISFJ?", "explain": "ISFJ 설명", "img1": "static/img/isfj.jpg", "img2": "static/img/esfp.png", "img3": "static/img/entj.jpg", "good": "독고혜지", "bad": "고세구"},
-    "ESTJ": {"character": "이덕수 할아바이", "mbti": "당신은 혹시... ESTJ?", "explain": "ESTJ 설명", "img1": "static/img/estj.png", "img2": "static/img/isfp.jpg", "img3": "static/img/infj.jpg", "good": "부정형 인간", "bad": "징버거 & 비챤"},
-    "ESFJ": {"character": "엔젤님", "mbti": "당신은 혹시... ESFJ?", "explain": "ESFJ 설명", "img1": "static/img/esfj.png", "img2": "static/img/istj.jpg", "img3": "static/img/intj.png", "good": "우왁굳", "bad": "냉참"},
-    "ISTP": {"character": "새우튀김", "mbti": "당신은 혹시... ISTP?", "explain": "ISTP 설명", "img1": "static/img/istp.png", "img2": "static/img/enfj.jpg", "img3": "static/img/enfp.jpg", "good": "도파민 박사", "bad": "릴파"},
-    "ISFP": {"character": "부정형 인간", "mbti": "당신은 혹시... ISFP?", "explain": "ISFP 설명", "img1": "static/img/isfp.jpg", "img2": "static/img/estj.png", "img3": "static/img/entp.png", "good": "이덕수 할아바이", "bad": "풍신"},
-    "ESTP": {"character": "김치만두번영택사스가", "mbti": "당신은 혹시... ESTP?", "explain": "ESTP 설명", "img1": "static/img/estp.png", "img2": "static/img/istj.jpg", "img3": "static/img/infp.jpg", "good": "우왁굳", "bad": "뢴트게늄 & 아이네"},
-    "ESFP": {"character": "독고혜지", "mbti": "당신은 혹시... ESFP?", "explain": "ESFP 설명", "img1": "static/img/esfp.png", "img2": "static/img/isfj.jpg", "img3": "static/img/intp.jpg", "good": "미츠네 하쿠", "bad": "주르르"},
-    "INFP": {"character": "뢴트게늄 & 아이네", "mbti": "당신은 혹시... INFP?", "explain": "INFP 설명", "img1": "static/img/infp.jpg", "img2": "static/img/entj.jpg", "img3": "static/img/estp.png", "good": "고세구", "bad": "김치만두번영택사스가"},
-    "INTP": {"character": "주르르", "mbti": "당신은 혹시... INTP?", "explain": "INTP 설명", "img1": "static/img/intp.jpg", "img2": "static/img/enfj.jpg", "img3": "static/img/esfp.png", "good": "도파민 박사", "bad": "독고혜지"},
-    "ENFP": {"character": "릴파", "mbti": "당신은 혹시... ENFP?", "explain": "ENFP 설명", "img1": "static/img/enfp.jpg", "img2": "static/img/intj.png", "img3": "static/img/istp.png", "good": "냉참", "bad": "새우튀김"},
-    "ENTP": {"character": "풍신", "mbti": "당신은 혹시... ENTP?", "explain": "ENTP 설명", "img1": "static/img/entp.png", "img2": "static/img/infj.jpg", "img3": "static/img/isfp.jpg", "good": "징버거 & 비챤", "bad": "부정형 인간"},
-    "INFJ": {"character": "징버거 & 비챤", "mbti": "당신은 혹시... INFJ?", "explain": "INFJ 설명", "img1": "static/img/infj.jpg", "img2": "static/img/entp.png", "img3": "static/img/estj.png", "good": "풍신", "bad": "이덕수 할아바이"},
-    "INTJ": {"character": "냉참", "mbti": "당신은 혹시... INTJ?", "explain": "INTJ 설명", "img1": "static/img/intj.png", "img2": "static/img/enfp.jpg", "img3": "static/img/esfj.png", "good": "릴파", "bad": "엔젤님"},
-    "ENFJ": {"character": "도파민 박사", "mbti": "당신은 혹시... ENFJ?", "explain": "ENFJ 설명", "img1": "static/img/enfj.jpg", "img2": "static/img/istp.png", "img3": "static/img/istj.jpg", "good": "새우튀김", "bad": "우왁굳"},
-    "ENTJ": {"character": "고세구", "mbti": "당신은 혹시... ENTJ?", "explain": "ENTJ 설명", "img1": "static/img/entj.jpg", "img2": "static/img/infp.jpg", "img3": "static/img/isfj.jpg", "good": "뢴트게늄 & 아이네", "bad": "미츠네 하쿠"}
+    "ISTJ": {"character": "우왁굳", "mbti": "당신은 혹시... ISTJ?", "explain": "ISTJ 설명", "img1": "static/img/istj.jpg", "img2": "static/img/esfj.png", "img3": "static/img/enfj.jpg", "good": "엔젤님", "bad": "도파민 박사", "presume": ""},
+    "ISFJ": {"character": "미츠네 하쿠", "mbti": "당신은 혹시... ISFJ?", "explain": "ISFJ 설명", "img1": "static/img/isfj.jpg", "img2": "static/img/esfp.png", "img3": "static/img/entj.jpg", "good": "독고혜지", "bad": "고세구", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "ESTJ": {"character": "이덕수 할아바이", "mbti": "당신은 혹시... ESTJ?", "explain": "ESTJ 설명", "img1": "static/img/estj.png", "img2": "static/img/isfp.jpg", "img3": "static/img/infj.jpg", "good": "부정형 인간", "bad": "징버거 & 비챤", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "ESFJ": {"character": "엔젤님", "mbti": "당신은 혹시... ESFJ?", "explain": "ESFJ 설명", "img1": "static/img/esfj.png", "img2": "static/img/istj.jpg", "img3": "static/img/intj.png", "good": "우왁굳", "bad": "냉참", "presume": ""},
+    "ISTP": {"character": "새우튀김", "mbti": "당신은 혹시... ISTP?", "explain": "ISTP 설명", "img1": "static/img/istp.png", "img2": "static/img/enfj.jpg", "img3": "static/img/enfp.jpg", "good": "도파민 박사", "bad": "릴파", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "ISFP": {"character": "부정형 인간", "mbti": "당신은 혹시... ISFP?", "explain": "ISFP 설명", "img1": "static/img/isfp.jpg", "img2": "static/img/estj.png", "img3": "static/img/entp.png", "good": "이덕수 할아바이", "bad": "풍신", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "ESTP": {"character": "김치만두번영택사스가", "mbti": "당신은 혹시... ESTP?", "explain": "ESTP 설명", "img1": "static/img/estp.png", "img2": "static/img/istj.jpg", "img3": "static/img/infp.jpg", "good": "우왁굳", "bad": "뢴트게늄 & 아이네", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "ESFP": {"character": "독고혜지", "mbti": "당신은 혹시... ESFP?", "explain": "ESFP 설명", "img1": "static/img/esfp.png", "img2": "static/img/isfj.jpg", "img3": "static/img/intp.jpg", "good": "미츠네 하쿠", "bad": "주르르", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "INFP": {"character": "뢴트게늄 & 아이네", "mbti": "당신은 혹시... INFP?", "explain": "INFP 설명", "img1": "static/img/infp.jpg", "img2": "static/img/entj.jpg", "img3": "static/img/estp.png", "good": "고세구", "bad": "김치만두번영택사스가", "presume": ""},
+    "INTP": {"character": "주르르", "mbti": "당신은 혹시... INTP?", "explain": "INTP 설명", "img1": "static/img/intp.jpg", "img2": "static/img/enfj.jpg", "img3": "static/img/esfp.png", "good": "도파민 박사", "bad": "독고혜지", "presume": ""},
+    "ENFP": {"character": "릴파", "mbti": "당신은 혹시... ENFP?", "explain": "ENFP 설명", "img1": "static/img/enfp.jpg", "img2": "static/img/intj.png", "img3": "static/img/istp.png", "good": "냉참", "bad": "새우튀김", "presume": ""},
+    "ENTP": {"character": "풍신", "mbti": "당신은 혹시... ENTP?", "explain": "ENTP 설명", "img1": "static/img/entp.png", "img2": "static/img/infj.jpg", "img3": "static/img/isfp.jpg", "good": "징버거 & 비챤", "bad": "부정형 인간", "presume": ""},
+    "INFJ": {"character": "징버거 & 비챤", "mbti": "당신은 혹시... INFJ?", "explain": "INFJ 설명", "img1": "static/img/infj.jpg", "img2": "static/img/entp.png", "img3": "static/img/estj.png", "good": "풍신", "bad": "이덕수 할아바이", "presume": ""},
+    "INTJ": {"character": "냉참", "mbti": "당신은 혹시... INTJ?", "explain": "INTJ 설명", "img1": "static/img/intj.png", "img2": "static/img/enfp.jpg", "img3": "static/img/esfj.png", "good": "릴파", "bad": "엔젤님", "presume": ""},
+    "ENFJ": {"character": "도파민 박사", "mbti": "당신은 혹시... ENFJ?", "explain": "ENFJ 설명", "img1": "static/img/enfj.jpg", "img2": "static/img/istp.png", "img3": "static/img/istj.jpg", "good": "새우튀김", "bad": "우왁굳", "presume": "* 해당 캐릭터의 유형은 제작자가 임의로 추정했습니다"},
+    "ENTJ": {"character": "고세구", "mbti": "당신은 혹시... ENTJ?", "explain": "ENTJ 설명", "img1": "static/img/entj.jpg", "img2": "static/img/infp.jpg", "img3": "static/img/isfj.jpg", "good": "뢴트게늄 & 아이네", "bad": "미츠네 하쿠", "presume": ""}
 }
 function start() { 
     $(".start").hide();
@@ -60,6 +60,7 @@ function next() {
         $("#character").html(result[mbti]["character"]);
         $("#mbti_text").html(result[mbti]["mbti"]);
         $("#explain").html(result[mbti]["explain"]);
+        $("#presume_text").html(result[mbti]["presume"]);
         $("#img2").attr("src", result[mbti]["img2"]);
         $("#img3").attr("src", result[mbti]["img3"]);
         $("#good").html(result[mbti]["good"]);
